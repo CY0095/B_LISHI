ebd6e18015aef136b183b152819eebf6bee3a1e6
ZZZZZP <feijidaichibang@aliyun.com>
2016-07-31 20:32:19 +0800

    7.31

---
+++ EPB_LiShi/Classes/Scenes/Views/MyCells/MyActivityCell.m
@@ -0,0 +1,23 @@
+//
+//  MyActivityCell.m
+//  EPB_LiShi
+//
+//  Created by lanou3g on 16/7/30.
+//  Copyright © 2016年 张鹏. All rights reserved.
+//
+
+#import "MyActivityCell.h"
+
+@implementation MyActivityCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
